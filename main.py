9eeea63ce0f57c4e804f0b101a2c7c1833e55d35
FakhrEldeen <fakhreldeengamal@gmail.com>
2017-12-03 23:05:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,35 @@ for i in f_lines:
     Parser_input.append(i.split(' : '))
 
 global row
+
 row=0
 
 def advance_input():
     global row
     row += 1
+    
+    
 def match(str):
+    global Error
     if Parser_input[row][0] == str:
         advance_input()
+        Error =0
 
     elif Parser_input[row][1] == str:
         advance_input()
+        Error =0
     else:
-        print ('ERROR found\n')
+        Error =1
 
 
 def read():
+    global Error
     match("read")
     match("ID")
-    
+    if Error == 0:
+        print("Read found!")
+    else:
+         print ('ERROR Found : Read statment is not correct \n')
 
 
 
